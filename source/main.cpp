00213f2b06508768a681e6523b00c9676ad66669
Barion <haquu17@gmail.com>
2021-09-27 22:09:06 +0600

    OpenGL and GLFW initialization

--- source/main.cpp
+++ source/main.cpp
@@ -3,21 +3,44 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int window_sizeX = 640;
+int window_sizeY = 480;
+
+void glfw_window_increasing_size(GLFWwindow* window, int width, int height)
+{
+    window_sizeX = width;
+    window_sizeY = height;
+    glViewport(0, 0, window_sizeX, window_sizeY);
+}
+
+void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE); 
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(window_sizeX, window_sizeY, "Battle city", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfw_window_increasing_size);
+    glfwSetKeyCallback(window, glfw_key_callback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -28,7 +51,8 @@ int main(void)
 		return -1;
 	}
 	
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 	
 	glClearColor(0, 1, 0.5, 1);
     /* Loop until the user closes the window */
@@ -36,6 +60,8 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
